b07146f3943dd836054d25046f214042970bcc00
Ninetailninja <ninetailninja@rocketmail.com>
2014-01-21 07:50:13 -0700

    added

---
+++ src/intro/hello.java
@@ -0,0 +1,15 @@
+package intro;
+
+public class hello
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello everyone my name is Michael Tuero.");
+
+	}
+
+}
